261db2ab3eedc88c7b6387a94390fa70237734f5
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-02-21 19:29:36 +0200

    added: yylex returns a list of the tokens

---
+++ lexical_analyzer/scanner.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct alpha_token_t {
+  unsigned int     numline;
+  unsigned int     numToken;
+  char          *content;
+  char          *type;
+  struct alpha_token_t *next;
+} alpha_token_t;
+
+unsigned int tokensCount = 0;
+
+alpha_token_t* alpha_token_t_create(unsigned int _numline, unsigned int _numToken, char *_content, char *_type);
+void add_token_to_list(alpha_token_t* token, alpha_token_t** head);
+char* deep_copy_string(const char* source);
