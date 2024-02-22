0c34c5387f5665a2c94fa340cc728478b13bf340
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-02-22 19:10:02 +0200

    added: regex for strings,real consts and ids

--- lexical_analyzer/scanner.h
+++ lexical_analyzer/scanner.h
@@ -15,3 +15,4 @@ unsigned int tokensCount = 0;
 alpha_token_t* alpha_token_t_create(unsigned int _numline, unsigned int _numToken, char *_content, char *_type);
 void add_token_to_list(alpha_token_t* token, alpha_token_t** head);
 char* deep_copy_string(const char* source);
+void check_escape_characters(char* source);
