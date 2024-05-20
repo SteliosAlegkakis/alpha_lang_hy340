9f6de90b95671e96077a5869a22e94d865a37b08
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-05-20 16:28:42 +0300

    added avm_structs.hpp

---
+++ avm/avm_structs.hpp
@@ -0,0 +1,40 @@
+enum vmopcode {
+    assign_v, add_v, sub_v, mul_v, div_v, mod_v, 
+    uminus_v, and_v, or_v, jeq_v, jne_v, jge_v, jgt_v, jlt_v, jle_v,
+    jmp_v, pusharg_v, funcenter_v, funcexit_v, call_v, newtable_v, 
+    tablegetelem_v, tablesetelem_v, jump_v, nop_v
+};
+
+enum vmarg_t{
+    label_a = 0, 
+    global_a = 1, 
+    formal_a = 2, 
+    local_a = 3, 
+    number_a = 4, 
+    string_a = 5, 
+    bool_a = 6, 
+    nil_a = 7, 
+    userfunc_a = 8, 
+    libfunc_a = 9, 
+    retval_a = 10
+};
+
+struct vmarg {
+    enum vmarg_t type;
+    unsigned val;
+};
+
+struct instruction {
+    enum vmopcode opcode;
+    struct vmarg  result;
+    struct vmarg  arg1;
+    struct vmarg  arg2;
+    unsigned      srcLine;
+};
+
+struct userfunc {
+    unsigned address;
+    unsigned localSize;
+    char*    id;
+};
+
