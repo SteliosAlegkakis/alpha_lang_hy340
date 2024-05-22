cef5a112f721dd06cac565929b9526f543481c0c
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-05-23 00:38:31 +0300

    refactored function prototypes

--- tcode_generator/tcode.hpp
+++ tcode_generator/tcode.hpp
@@ -1,9 +1,4 @@
-struct incomplete_jump{
-    unsigned         instrNo;
-    unsigned         iaddress;
-};
-
 void tcode_generate(void);
-void print_instructions(void);
+void tcode_print_instructions(void);
 void tcode_generate_binary(char* filename);
 
