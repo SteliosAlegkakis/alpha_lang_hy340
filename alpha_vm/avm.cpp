73822111d5aa8e2637b0f312b7eb4c2c1321724a
SteliosAlegkakis <stelios.alegak@gmail.com>
2024-06-06 21:27:28 +0300

    moved avm_assign to avm.cpp

--- alpha_vm/avm.cpp
+++ alpha_vm/avm.cpp
@@ -85,6 +85,27 @@ void avm_memcellclear(avm_memcell* m) {
     }
 }
 
+void avm_assign(avm_memcell* lv, avm_memcell* rv) {
+    if (lv == rv) {
+        return;
+    }
+    if (lv->type == table_m && rv->type == table_m && lv->data.tableVal == rv->data.tableVal) {
+        return;
+    }
+    if (rv->type == undef_m) {
+        avm_warning((char*)"assigning from 'undef' content!");
+    }
+    
+    avm_memcellclear(lv);
+
+    memcpy(lv, rv, sizeof(avm_memcell));
+    if (lv->type == string_m) {
+        lv->data.strVal = strdup(rv->data.strVal);
+    } else if (lv->type == table_m) {
+        avm_tableincref_counter(lv->data.tableVal);
+    }
+}
+
 avm_table* avm_tablenew(void) {
     avm_table* t = (avm_table*)malloc(sizeof(avm_table));
     AVM_WIPEOUT(*t);
